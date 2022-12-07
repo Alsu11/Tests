de0b97295587999dc53476a3dbc347f5fb2990cd
daniskzn <danis.sharafiev@mircod.com>
2022-12-07 19:49:13 +0300

    fix test4

--- Tests3/src/main/java/org/example/ApplicationManager.java
+++ Tests3/src/main/java/org/example/ApplicationManager.java
@@ -1,11 +1,8 @@
 package org.example;
 
-import org.example.helpers.LoginHelper;
-import org.example.helpers.MessageHelper;
-import org.example.helpers.NavigationHelper;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,7 +14,7 @@ public class ApplicationManager {
     private LoginHelper loginHelper;
     private MessageHelper messageHelper;
     private NavigationHelper navigationHelper;
-    private static ThreadLocal<ApplicationManager> app = new ThreadLocal<>();
+
     public ApplicationManager() {
         System.setProperty("webdriver.chrome.driver", "/home/user/Рабочий стол/chromedriver");
         driver = new ChromeDriver();
@@ -28,23 +25,9 @@ public class ApplicationManager {
         navigationHelper = new NavigationHelper(this);
     }
 
-    protected void finalize() {
-        try {
-            driver.quit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    protected void stop() {
+        driver.quit();
     }
-
-    public static ApplicationManager getInstance() {
-        if(app.get() == null) {
-            ApplicationManager applicationManager = new ApplicationManager();
-            applicationManager.getNavigationHelper().openHomePage();
-            app.set(applicationManager);
-        }
-        return app.get();
-    }
-
 
     public LoginHelper getLoginHelper() {
         return loginHelper;
