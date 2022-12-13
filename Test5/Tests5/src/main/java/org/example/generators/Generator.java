0d28a6cb92a84190ee6d50aa34fb817b90f8ef8b
daniskzn <danis.sharafiev@mircod.com>
2022-12-14 00:04:17 +0300

    fix generator

--- Test5/Tests5/src/main/java/org/example/generators/Generator.java
+++ Test5/Tests5/src/main/java/org/example/generators/Generator.java
@@ -13,7 +13,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
-public class DomGenerator {
+public class Generator {
 
     public static void main(String[] args) {
 
